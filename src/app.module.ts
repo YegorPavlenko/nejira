4a6ef76c0d8650b53565cca5fed381b68ce99766
YegorPavlenko <yegor.pavlenko@gmail.com>
2022-11-13 17:39:32 +0200

    build(config): add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
+import { AttachmentsModule } from './attachments/attachments.module';
+import { CommentsModule } from './comments/comments.module';
+import { FeaturesModule } from './features/features.module';
+import { MilestonesModule } from './milestones/milestones.module';
+import { TasksModule } from './tasks/tasks.module';
+import { EmployeesModule } from './employees/employees.module';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [],
+  imports: [ProjectsModule, AttachmentsModule, CommentsModule, FeaturesModule, MilestonesModule, TasksModule, EmployeesModule, HistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
