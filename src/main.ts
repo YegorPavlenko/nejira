24e06de87f89607a1e8151db613c25cb756001a1
YegorPavlenko <yegor.pavlenko@gmail.com>
2022-12-05 17:35:08 +0200

    feat: add "OpenaAPI" (Swagger) doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
+
+  const config = new DocumentBuilder()
+    .setTitle('Nejira demo')
+    .setDescription('Nejira demo API description')
+    .setVersion('0.0.1')
+    .addTag('nejira')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
